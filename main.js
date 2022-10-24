b9098d2e967aae9795464c4afb6c95fb4eacbde8
Azgalar <sergiolara88@hotmail.com>
2022-10-24 00:34:52 -0400

    feature header

--- main.js
+++ main.js
@@ -1,23 +1,18 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+const hamburger = document.querySelector(".nav__hamburger");
+const navMenu = document.querySelector(".nav__navbar");
+const navLink = document.querySelectorAll(".nav__link");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+hamburger.addEventListener("click", mobileMenu);
 
-setupCounter(document.querySelector('#counter'))
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
